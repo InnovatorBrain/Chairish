d95ae31b02d7471e3988a0711bed3b69228ebe19
6545 FAIZAN AHMED <fazitanveer2@gmail.com>
2023-02-21 21:18:19 +0500

    Data interaction insertion and deletion (shell)

--- myapp/models.py
+++ myapp/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 # model for checkout page.
+
+
 class my_checkout(models.Model):
     Country = models.CharField(max_length=20)
     fname = models.CharField(max_length=15)
@@ -14,3 +16,9 @@ class my_checkout(models.Model):
     Phone = models.IntegerField()
     order_notes = models.TextField()
     Coupon_Code = models.CharField(max_length=20)
+# def for showing in shell the database instance in a string way of this my_checkout model
+    def __str__(self):
+        return f"{self.fname}{self.lname} lives in {self.Country}."
+
+# filter is for getting specific results 
+# Q is for capturing more complex results
