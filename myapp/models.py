4a114aa51e97adbd0fbe12e6592fc6ce8b7c5e7f
6545 FAIZAN AHMED <fazitanveer2@gmail.com>
2023-02-25 09:20:08 +0500

    Checkout play_ground

--- myapp/models.py
+++ myapp/models.py
@@ -1,24 +1,25 @@
 from django.db import models
 
-# Create your models here.
-# model for checkout page.
+# # Create your models here.
+# # model for checkout page.
 
 
-class my_checkout(models.Model):
-    Country = models.CharField(max_length=20)
+class Checkout(models.Model):
+    country = models.CharField(max_length=20)
     fname = models.CharField(max_length=15)
     lname = models.CharField(max_length=15)
-    companyname = models.CharField(max_length=20)
-    address = models.TextField(max_length=50)
-    state_country = models.CharField(max_length=20)
-    postal_zip = models.IntegerField(max_length=10)
-    email_address = models.EmailField()
-    Phone = models.IntegerField()
-    order_notes = models.TextField()
-    Coupon_Code = models.CharField(max_length=20)
-# def for showing in shell the database instance in a string way of this my_checkout model
-    def __str__(self):
-        return f"{self.fname}{self.lname} lives in {self.Country}."
+    companyname = models.CharField(max_length=20, blank=True, null=True)  # use blank=True, null=True instead of required=False
+    address = models.CharField(max_length=50)
+    state_or_province = models.CharField(max_length=20)
+    postal_code = models.CharField(max_length=10)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    order_notes = models.CharField(max_length=500, blank=True, null=True)  # increase max_length and use blank=True, null=True instead of required=False
+    coupon_code = models.CharField(max_length=20, blank=True, null=True)
 
-# filter is for getting specific results 
-# Q is for capturing more complex results
+# # def for showing in shell the database instance in a string way of this my_checkout model
+#     def __str__(self):
+#         return f"{self.fname}{self.lname} lives in {self.Country}."
+
+# # filter is for getting specific results 
+# # Q is for capturing more complex results
--- myapp/views.py
+++ myapp/views.py
@@ -1,32 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse
+from .models import Checkout
+
 
 def index(request):
     return render(request, "index.html")
 
+
 def about(request):
     return render(request, 'about.html')
+
 
 def blog(request):
     return render(request, 'blog.html')
 
+
 def cart(request):
     return render(request, 'cart.html')
 
+
+
 def checkout(request):
-    return render(request, 'checkout.html')
+    # Post Request >> form content >> Thanks
+    if request.method == "POST":
+        form = Checkout(request.POST)
+        if form.is_valid():
+            # Save form data to database using Checkout model
+            checkout = Checkout(
+                country=form.cleaned_data["country"],
+                fname=form.cleaned_data["fname"],
+                lname=form.cleaned_data["lname"],
+                companyname=form.cleaned_data.get("companyname", None),
+                address=form.cleaned_data["address"],
+                state_or_province=form.cleaned_data["state_or_province"],
+                postal_code=form.cleaned_data["postal_code"],
+                email=form.cleaned_data["email"],
+                phone=form.cleaned_data["phone"],
+                order_notes=form.cleaned_data.get("order_notes", None),
+                coupon_code=form.cleaned_data.get("coupon_code", None)
+            )
+            checkout.save()
+            return redirect(reverse("myapp:checkout"))
+    # Else render form
+    else:
+        form = Checkout()
+    return render(request, "checkout.html", context={"form": form})
 
 def contact(request):
     return render(request, 'contact.html')
 
+
 def services(request):
     return render(request, 'services.html')
+
 
 def shop(request):
     return render(request, 'shop.html')
 
+
 def thankyou(request):
     return render(request, 'thankyou.html')
 
-# def handler404(request, exception):              >>> for error msg code having error 
+# def handler404(request, exception):              >>> for error msg code having error
 #     return render(request, '404.html', status=404)
